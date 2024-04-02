cad89a7e841c32db2343bdb08686d16701d4e0cd
Hyunchel47 <127382137+Hyunchel47@users.noreply.github.com>
2024-04-02 22:56:21 +0900

    Add files via upload

---
+++ Chapter3/Chapter3 실습1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+9세 이상이면서, 키가 130cm 이상인 어린이는 "고속 롤러코스트 입장 가능",
+9세 이상이면서, 키가 130cm 미만인 어린이는 "저속 롤러코스트 입장 가능",
+9세 미만인 어린이는 "입장 불가!"를 출력하는 프로그램 작성
+*/
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("나이 입력 : ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 9)
+        {
+            Console.Write("키 입력 : ");
+            int height = int.Parse(Console.ReadLine());
+
+            if (height >= 130)
+            {
+                Console.WriteLine("고속 롤러코스트 입장 가능");
+            }
+            else
+            {
+                Console.WriteLine("저속 롤러코스트 입장 가능");
+            }
+        }
+        else
+        {
+            Console.WriteLine("입장불가!");
+        }
+
+    }
+}
---
+++ Chapter3/Chapter3 예제2/Program.cs
@@ -0,0 +1,33 @@
+﻿// 2자리의 정수(10~99 사이)를 입력 받아 십의 자리와 1의 자리가 같은지  판별하여 출력
+internal class Program
+{
+    private static void Main(string[] args)
+    { 
+        Console.Write("2자리 정수 입력 (10 ~ 99) : ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num >= 10)
+        {
+            if (num < 100)
+            {
+                if (num % 11 == 0)
+                {
+                    Console.WriteLine("Yes! 10의 자리와 1의 자리가 같습니다.");
+                }
+                else
+                {
+                    Console.WriteLine("No! 10의 자리와 1의 자리가 다릅니다.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("10 ~ 99 사이의 정수만 입력하세요.");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("10 ~ 99 사이의 정수만 입력하세요.");
+        }
+    }
+}
